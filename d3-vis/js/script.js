26589652a6d38a6db288660a96ad7d63b0bc060f
Christopher Wells <cwellsny@nycap.rr.com>
2021-01-23 22:50:54 -0800

    Implement d3 hello world

--- d3-vis/js/script.js
+++ d3-vis/js/script.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+function main() {
+    let vis_svg = d3.select("#character-visualization")
+        .append("svg");
+
+    vis_svg.append("rect")
+        .attr("fill", "blue")
+        .attr("width", 100)
+        .attr("height", 200);
+}
